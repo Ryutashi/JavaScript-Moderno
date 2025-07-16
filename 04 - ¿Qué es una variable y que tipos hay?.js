aefcf8f417fd597e3fc9c557e25f3487a03d4589
Ryutashi <145888578+Ryutashi@users.noreply.github.com>
2025-07-16 14:11:48 -0500

    04 - ¿Qué es una variable y que tipos hay?.js

---
+++ 04 - ¿Qué es una variable y que tipos hay?.js
@@ -0,0 +1,21 @@
+// VARIABLES
+
+/* En la programación una variable es una caja o contenedor donde puedes guardar datos o información, y es semejante por ejemplo
+a una caja a la cual le pones una etiqueta para identificar su contenido. 
+
+Puedes tener una variable llamada "zapatos" y dentro de esa variable tener datos de esos zapatos, como guardar el número de zapatos que tienes, por ejemplo 5.
+
+En una variable se pueden guardar cualquier tipo de dato, como números, cadenas de texto, booleanos, objetos, entre otros.*/
+
+
+var nombre = "Jose Luis";
+/* var tiene un alcance mas amplio, ya que se puede llamar esa variable y modificarla en cualquier parte del código, pero a veces suele causar ciertos problemas
+o confusiones, pero es la manera clásica de definir una variable */
+
+let apellido = "Cruz Aguirre";
+/* let tiene un alcance más limitado, ya que solo se puede llamar y modificar esa variable dentro del bloque de código donde fue definida,
+y permite hacer mejores prácticas que con var */
+
+const nacionalidad = "Colombia";
+/* const es una variable que no se puede modificar una vez definida, es decir, su valor es constante. Se usa para valores que no cambian a lo largo del tiempo,
+como por ejemplo la nacionalidad, que no cambia una vez definida. */
