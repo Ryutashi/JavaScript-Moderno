93a3d21f0c454e853b71b526528bb54290d95e36
Ryutashi <145888578+Ryutashi@users.noreply.github.com>
2025-07-16 13:19:44 -0500

    03 - Un script aparte.js

---
+++ 03 - Un script aparte.js
@@ -0,0 +1,14 @@
+// Como buena práctica, lo mejor es separar el código JavaScript en un archivo externo
+// y enlazarlo en el HTML de la siguiente manera:
+// <script src="main.js"></script>
+// En este caso, el archivo se llama main.js y está en la misma carpeta que el HTML
+
+
+alert("Hola que tal, bienvenido al curso");
+alert("Estamos aprendiendo JS");
+alert("JavaScript es el lenguaje más demandado");
+
+console.log("Soy un mensaje enviado desde JS");
+
+// También podemos hacer operaciones matemáticas y mostrarlas en la consola
+console.log(124 + 11); /*135*/
