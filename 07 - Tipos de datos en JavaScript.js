046de0e2a004b812976d6e38a4a4c981f7f7157c
Ryutashi <145888578+Ryutashi@users.noreply.github.com>
2025-07-18 11:43:34 -0500

    07 - Tipos de datos en JavaScript.js

---
+++ 07 - Tipos de datos en JavaScript.js
@@ -0,0 +1,19 @@
+// TIPOS DE DATOS
+
+
+let frase = "Hola, soy una cadena de texto"; // String o cadena de texto
+
+let anio = 2027; // Number o número entero
+
+let precio = 19.99; // Number o número decimal / float / double
+
+let mayorDeEdad = true; // Booleano (true o false)
+
+let vacio;  // undefined
+
+let nulo = null; // null representa la ausencia de valor
+
+let frutas = ["manzana", "banana", "naranja"]; // Array o arreglo (lista de elementos)
+
+let heroe = { nombre: "Batman", universo: "DC Comics" }; // Object o objeto (colección de propiedades y valores)
+
