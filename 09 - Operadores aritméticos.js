4e747a465733738d6c049f750db4cfa9955827f7
Ryutashi <145888578+Ryutashi@users.noreply.github.com>
2025-07-18 15:53:58 -0500

    09 - Operadores aritméticos.js

---
+++ 09 - Operadores aritméticos.js
@@ -0,0 +1,42 @@
+// OPERADORES
+
+// OPERADORES ARITMÉTICOS
+
+/*
+Los operadores aritméticos se utilizan para realizar operaciones matemáticas básicas. Los más comunes son:
+- Suma (+)
+- Resta (-)
+- Multiplicación (*)
+- División (/)
+- Módulo (%)
+*/
+
+let numero1 = 12;
+
+let numero2 = 44;
+
+let suma = numero1 + numero2; // Suma
+
+let resta = numero1 - numero2; // Resta
+
+let multiplicacion = numero1 * numero2; // Multiplicación
+
+let division = numero1 / numero2; // División
+
+let modulo = numero1 % numero2; // Módulo (resto de la división)
+
+let potencia = numero1 ** 2; // Potencia (numero1 elevado a la 2)
+
+
+
+console.log("Suma:", suma); // Imprime: Suma: 56
+
+console.log("Resta:", resta); // Imprime: Resta: -32
+
+console.log("Multiplicación:", multiplicacion); // Imprime: Multiplicación: 528
+
+console.log("División:", division); // Imprime: División: 0.2727272727272727
+
+console.log("Módulo:", modulo); // Imprime: Módulo: 12
+
+console.log("Potencia:", potencia); // Imprime: Potencia: 144
