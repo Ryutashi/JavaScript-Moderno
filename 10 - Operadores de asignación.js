62cb00bc876df63ca87c4464cdf595903b200bcb
Ryutashi <145888578+Ryutashi@users.noreply.github.com>
2025-07-18 16:34:46 -0500

    10 - Operadores de asignación.js

---
+++ 10 - Operadores de asignación.js
@@ -0,0 +1,34 @@
+// OPERADORES
+
+// OPERADORES DE ASIGNACIÓN
+
+
+// Los operadores de asignación se utilizan para asignar valores a variables. El más común es el operador de asignación simple (=).
+
+let a = 5; // Asignación simple
+
+let b = 10;
+
+let c = a + b; // Asignación con expresión
+
+
+
+
+let numero = 17;
+
+numero = 17 + 3; // Asignación con suma
+
+/* Utilizamos "console.warn" para que sea más fácil ver los cambios en el valor de la variable en la consola, al igual que "console.error" */
+
+console.warn("Número después de asignaciones:", numero); // Imprime: Número después de asignaciones: 20
+
+
+numero += 3; // Asignación con suma abreviada (equivalente a numero = 17 + 3)
+
+numero -= 3; // Asignación con resta abreviada (equivalente a numero = 17 - 3)
+
+numero *= 2; // Asignación con multiplicación abreviada (equivalente a numero = 17 * 2)
+
+numero /= 2; // Asignación con división abreviada (equivalente a numero = 17 / 2)
+
+numero %= 5; // Asignación con módulo abreviado (equivalente a numero = 17 % 5)
