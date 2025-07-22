590225c9aadcaa57121fc0ca6d770b0e9ae56d1d
Ryutashi <145888578+Ryutashi@users.noreply.github.com>
2025-07-22 12:25:13 -0500

    13 -  Operadores de incremento y decremento.js

---
+++ 13 -  Operadores de incremento y decremento.js
@@ -0,0 +1,55 @@
+// OPERADORES DE INCREMENTO Y DECREMENTO
+
+/* El operador de incremento (++) se utiliza para aumentar el valor de una variable en 1, y el operador de decremento (--) se utiliza para disminuir el valor de una variable en 1.*/
+
+
+let cifra = 1200;
+
+
+// cifra = 1200 + 1;
+
+// cifra = cifra + 1; 
+
+
+// (++)
+cifra++; // Incremento
+
+console.log(cifra); //1201, ya que se ha incrementado en 1
+
+
+
+cifra++;
+cifra++;
+cifra++;
+cifra++;
+cifra++;
+
+console.log(cifra); //1206, ahora ha incrementado en 5
+
+
+
+
+
+
+
+// cifra = 1200 - 1;
+
+// cifra = cifra - 1; 
+
+
+// (--)
+cifra--; // Decremento
+
+console.log(cifra); // 1205, ya que se ha incrementado anteriormente en +6 y decrementado en -1
+
+
+cifra--;
+cifra--;
+cifra--;
+cifra--;
+cifra--;
+
+console.log(cifra); // 1200, el valor se ha decrementado en -5
+
+
+
