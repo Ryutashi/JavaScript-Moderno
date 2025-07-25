e4ff100b5b23afd04c2cad2cfbb169f98543d590
Ryutashi <145888578+Ryutashi@users.noreply.github.com>
2025-07-25 12:32:30 -0500

    20 - Condicional Ternario.js

---
+++ 20 - Condicional Ternario.js
@@ -0,0 +1,30 @@
+// ESTRUCTURAS DE CONTROL
+
+
+// MÁS CONDICIONALES
+
+
+
+
+// CONDICIONAL TERNARIO 
+
+
+/* Nos permite hacer una condición de manera más rápida y en una sola linea de código.
+Este tipo de expresión lo podemos utilizar en muchos casos en el desarrollo de cualquier tipo de aplicación,
+para evaluar condiciones de manera más rápida */
+
+
+let nombre = "Cloud Strife";
+
+let edad = 17;
+
+
+/* En los parentesis () colocamos la condición, luego despues de la interrogación (?), colocamos el resultado o lo que nos va a devolve en (""),
+siendo el primero "true", "Es mayor de edad" si se cumple la condición, y despues de (:) 
+si no se cumple la condición, sería "false", "Es menor de edad" */
+
+let resultado = (edad >= 18) ? "Es mayor de edad" : "Es menor de edad";
+
+
+console.log(resultado); // Es menor de edad
+
