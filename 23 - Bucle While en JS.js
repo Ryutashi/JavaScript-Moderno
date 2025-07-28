1d8bad833af557022296334b111bc48bf0c359fc
Ryutashi <145888578+Ryutashi@users.noreply.github.com>
2025-07-28 12:55:58 -0500

    23 - Bucle While en JS.js

---
+++ 23 - Bucle While en JS.js
@@ -0,0 +1,58 @@
+// ESTRUCTURAS DE CONTROL
+
+
+
+// BUCLE WHILE
+
+
+/* El bucle "while" se utiliza para ejecutar un bloque de código mientras una condición se cumpla o sea verdadera.
+A diferencia del bucle "for", el bucle "while" no requiere una inicialización, una condición de finalización o un incremento en su declaración inicial, ya que el incremento y la condición se manejan dentro del bloque de código.
+
+ Ejemplo:
+
+ while (condición) {
+    bloque de código a repetir
+
+}
+
+*/
+
+
+
+// EJEMPLO 1
+
+
+let year = 1990; // inicialización fuera del while
+
+let objetivo = 2177;
+
+let interferencia = 2117;
+
+
+/* Creamos la variable "year" que se inicializa en 1990, luego en el bucle while le asignamos la condición de que si "year" es diferente (!=) de la variable "objetivo", se incremente en 1 hasta llegar al objetivo de 2177, luego en el bloque de código asignamos la variable "year" con el operador de incremento (++) al final. */
+
+while (year != objetivo) {
+    year++; // incremento dentro del bloque
+
+    console.log(`Estamos ahora en el año ${year}`); // Imprime los años desde 1991 hasta 2177
+} 
+
+
+
+
+// EJEMPLO 2
+
+
+/* Tambien podemos cortar el bucle donde queramos, por ejemplo con un "if" dentro del bloque, y comprobamos que si "year" (1990) es igual (===) a "interferencia" (2117), se detenga el bucle en el año 2117 con un "break". */
+
+while (year != objetivo) {
+    year++;
+    console.log(`Estamos ahora en el año ${year}`); // Imprime los años desde 1991 hasta 2177
+    
+    
+    
+    if (year === interferencia) {
+        console.log(`¡Atención! En el año ${year} hay una interferencia.`); // ¡Atención! En el año 2117 hay una interferencia.
+        break;
+    }   
+}
