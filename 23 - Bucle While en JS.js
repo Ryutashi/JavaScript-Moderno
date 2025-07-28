4fbfd0b62f629879df563852b6b6c10836da3ba6
Ryutashi <145888578+Ryutashi@users.noreply.github.com>
2025-07-28 13:04:07 -0500

    23 - Bucle While en JS.js

--- 23 - Bucle While en JS.js
+++ 23 - Bucle While en JS.js
@@ -6,21 +6,21 @@
 
 
 /* El bucle "while" se utiliza para ejecutar un bloque de código mientras una condición se cumpla o sea verdadera.
-A diferencia del bucle "for", el bucle "while" no requiere una inicialización, una condición de finalización o un incremento en su declaración inicial, ya que el incremento y la condición se manejan dentro del bloque de código.
+A diferencia del bucle "for", el bucle "while" no requiere una inicialización, una condición de finalización o un incremento en su declaración inicial, 
+ya que el incremento se manejan dentro del bloque de código.
 
  Ejemplo:
 
  while (condición) {
     bloque de código a repetir
 
-}
-
-*/
+} */
 
 
 
 // EJEMPLO 1
 
+// Creamos la variable "year" que se inicializa en 1990
 
 let year = 1990; // inicialización fuera del while
 
@@ -29,7 +29,8 @@ let objetivo = 2177;
 let interferencia = 2117;
 
 
-/* Creamos la variable "year" que se inicializa en 1990, luego en el bucle while le asignamos la condición de que si "year" es diferente (!=) de la variable "objetivo", se incremente en 1 hasta llegar al objetivo de 2177, luego en el bloque de código asignamos la variable "year" con el operador de incremento (++) al final. */
+/* En el bucle "while" le asignamos la condición de que si "year" es diferente (!=) de la variable "objetivo", se incremente en 1 hasta llegar al objetivo de 2177,
+luego en el bloque de código asignamos la variable "year" con el operador de incremento (++) al final. */
 
 while (year != objetivo) {
     year++; // incremento dentro del bloque
@@ -43,7 +44,8 @@ while (year != objetivo) {
 // EJEMPLO 2
 
 
-/* Tambien podemos cortar el bucle donde queramos, por ejemplo con un "if" dentro del bloque, y comprobamos que si "year" (1990) es igual (===) a "interferencia" (2117), se detenga el bucle en el año 2117 con un "break". */
+/* Tambien podemos cortar el bucle donde queramos, por ejemplo con un "if" dentro del bloque, 
+y comprobamos que si "year" (1990) es igual (===) a "interferencia" (2117), se detenga el bucle en el año 2117 con un "break". */
 
 while (year != objetivo) {
     year++;
